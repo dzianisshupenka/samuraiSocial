36f6e51e3d813c81851d2b9977b89b32ea987260
Dzianis <imxrayz@gmail.com>
2020-04-25 20:13:04 +0300

    connect redux devTool

--- src/redux/reduxStore.js
+++ src/redux/reduxStore.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import profileReducer from "./profileReducer";
 import dialogReducer from "./dialogReducer";
 import usersReducer from "./usersReducer";
@@ -16,8 +16,9 @@ let reducers = combineReducers ({
     app: appReducer
 })
 
-let store = createStore(reducers, applyMiddleware(thunkMiddleWare));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleWare)));
 
-window.store = store;
+//window.store = store;
 
 export default store;
