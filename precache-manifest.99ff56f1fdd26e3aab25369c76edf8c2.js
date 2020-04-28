722cf04909bc4b97c764fa3eb874dfd01744ae8e
Dzianis <imxrayz@gmail.com>
2020-04-28 14:11:17 +0300

    Updates

--- precache-manifest.99ff56f1fdd26e3aab25369c76edf8c2.js
+++ precache-manifest.99ff56f1fdd26e3aab25369c76edf8c2.js
@@ -1,43 +1,43 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5d3df59000d3bb4299f44e8f469b9a8d",
+    "revision": "ee90c385d8a28bb092db613bf0396ab1",
     "url": "/samuraiSocial/index.html"
   },
   {
-    "revision": "7a63aa198fa380189f6e",
+    "revision": "e03a9f7a9abccd1a2e49",
     "url": "/samuraiSocial/static/css/3.03235744.chunk.css"
   },
   {
-    "revision": "a42c7412bc0b5547edf3",
+    "revision": "0f042fc483908e67e7fb",
     "url": "/samuraiSocial/static/css/4.de52e3d2.chunk.css"
   },
   {
-    "revision": "7c112436f23549f9e46b",
+    "revision": "e93c99057c9e4985607c",
     "url": "/samuraiSocial/static/css/main.f409183d.chunk.css"
   },
   {
-    "revision": "40bc2a88a5dc4ce085b2",
-    "url": "/samuraiSocial/static/js/2.e553652e.chunk.js"
+    "revision": "d882a61dda9c12257897",
+    "url": "/samuraiSocial/static/js/2.2e5d8a0d.chunk.js"
   },
   {
     "revision": "f231859d6585c4cd5f80c344783ed269",
-    "url": "/samuraiSocial/static/js/2.e553652e.chunk.js.LICENSE.txt"
+    "url": "/samuraiSocial/static/js/2.2e5d8a0d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7a63aa198fa380189f6e",
-    "url": "/samuraiSocial/static/js/3.7948c059.chunk.js"
+    "revision": "e03a9f7a9abccd1a2e49",
+    "url": "/samuraiSocial/static/js/3.d42ffa4b.chunk.js"
   },
   {
-    "revision": "a42c7412bc0b5547edf3",
-    "url": "/samuraiSocial/static/js/4.0c42fa5f.chunk.js"
+    "revision": "0f042fc483908e67e7fb",
+    "url": "/samuraiSocial/static/js/4.7cd2a6a6.chunk.js"
   },
   {
-    "revision": "7c112436f23549f9e46b",
-    "url": "/samuraiSocial/static/js/main.ab73319f.chunk.js"
+    "revision": "e93c99057c9e4985607c",
+    "url": "/samuraiSocial/static/js/main.c167b215.chunk.js"
   },
   {
-    "revision": "4136740d7cc1e017693f",
-    "url": "/samuraiSocial/static/js/runtime-main.4affe38d.js"
+    "revision": "a2984565443fc954bca1",
+    "url": "/samuraiSocial/static/js/runtime-main.bc8d1934.js"
   },
   {
     "revision": "76fdbddc675c9261f0dd692db29db3fd",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/samuraiSocial/precache-manifest.ed0d857ddd22f094ec4fed030108fcf8.js"
+  "/samuraiSocial/precache-manifest.99ff56f1fdd26e3aab25369c76edf8c2.js"
 );
 
 self.addEventListener('message', (event) => {
